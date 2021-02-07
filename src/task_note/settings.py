681db52c274b96dfb3ffcdb62dfb555d12ecf123
tsuchiya <youtaro1123@gmail.com>
2021-02-07 21:51:47 +0900

    Implement static and routing

--- src/task_note/settings.py
+++ src/task_note/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'note.apps.NoteConfig',
 ]
 
 MIDDLEWARE = [
@@ -132,4 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static')
+)
